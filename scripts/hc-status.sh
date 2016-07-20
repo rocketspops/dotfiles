91a0f03d8757cd2a4448ad856e1908e939a62ffd
rocketspops <rocketspops@gmail.com>
2016-07-20 12:59:59 -0500

    update instructions for hc-status.sh

--- scripts/hc-status.sh
+++ scripts/hc-status.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # USAGE
-# 1. Install Homebrew (http://brew.sh/)
-# 2. Install blink1-tool: $ brew install blink1
-# 3. Grab a copy of hc-status.sh (https://git.io/vK6Ao) and place it in ~/bin
-# 4. Grab a copy of hc-status.cfg (https://git.io/vK6x3) and place it in ~/bin
-# 5. Create 2 personal API tokens scoped to Admin Group and View Group  (https://www.hipchat.com/sign_in?d=%2Faccount%2Fapi)
-# 6. Update hc-status.cfg with your API tokens
-# 5. Get your current user info: $ sh hc-status.sh get
-# 6. Update the rest of hc-status.cfg with correct user info
-# 7. Update your status and Blink1 light: $ sh hc-status.sh chat "I'm available to answer questions."
+#  1. Install Homebrew (http://brew.sh/)
+#  2. Install blink1-tool: $ brew install blink1
+#  3. Create a /bin file in your home directory: $ mkdir ~/bin
+#  4. Grab a copy of hc-status.sh (https://git.io/vK6Ao) and place it in ~/bin
+#  5. Grab a copy of hc-status.cfg (https://git.io/vK6x3) and place it in ~/bin
+#  6. Add ~/bin to your $PATH (http://www.troubleshooters.com/linux/prepostpath.htm#_singleuser)
+#  7. Create 2 personal API tokens scoped to Admin Group and View Group  (https://www.hipchat.com/sign_in?d=%2Faccount%2Fapi)
+#  8. Update hc-status.cfg with your API tokens
+#  9. Get your current user info: $ sh hc-status.sh get
+# 10. Update the rest of hc-status.cfg with correct user info
+# 11. Update your status and Blink1 light: $ sh hc-status.sh chat "I'm available to answer questions."
 
 source ~/bin/hc-status.cfg
 
