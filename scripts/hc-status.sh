3db0604210214025be56233b9eedae225f54f59d
Billy Whited <billy.whited@ampf.com>
2016-11-11 11:32:01 -0600

    simplify status detection

--- scripts/hc-status.sh
+++ scripts/hc-status.sh
@@ -11,7 +11,8 @@ blink1_devices=$(blink1-tool --list | grep -q "id:[0-9]\+"; echo $?)
 
 while  [ "${blink1_devices}" == 0 ]; do
 
-  echo "$(tput bold)Fetching user object via HipChat API...$(tput sgr0)"
+  echo "$(tput bold)Checking user object via HipChat API...$(tput sgr0)"
+  echo "Last user show: ${user_last_show}"
 
   get_user=$(curl \
              --silent \
@@ -21,48 +22,33 @@ while  [ "${blink1_devices}" == 0 ]; do
             )
 
   user_presence=$(echo ${get_user} | jq -r ".presence.is_online")
-  user_status=$(echo ${get_user} | jq -r ".presence.show")
-
-  get_last_color=$(blink1-tool --rgbread  --quiet)
-  last_color=$(printf "rgb(%d,%d,%d)\n" ${get_last_color//,/ })
+  user_current_status=$(echo ${get_user} | jq -r ".presence.status")
+  user_current_show=$(echo ${get_user} | jq -r ".presence.show")
 
   if [ "${user_presence}" == "true" ]; then
 
-    echo "${user_presence}"
-    echo "${user_status}"
-    echo "${last_color}"
+    echo "Current user show: ${user_current_show}"
 
-    case ${user_status} in
-      "chat")
-        if [ "${last_color}" != "rgb(0,255,0)" ]; then
-          blink1-tool --rgb=0,255,0 --nogamma --quiet
-          echo "Your status has changed to: ${user_status}"
-        fi
-        ;;
-      "away"|"xa")
-        if [ "${last_color}" != "rgb(255,100,0)" ]; then
-          blink1-tool --rgb=255,100,0 --nogamma --quiet
-          echo "Your status has changed to: ${user_status}"
-        fi
-        ;;
-      "dnd")
-        if [ "${last_color}" != "rgb(255,0,0)" ]; then
-          blink1-tool --rgb=255,0,0 --nogamma --quiet
-          echo "Your status has changed to: ${user_status}"
-        fi
-        ;;
-      *)
-        echo "..."
-        ;;
-    esac
+    if [ "${user_current_show}" != "${user_last_show}" ]; then
+
+      case ${user_current_show} in
+        "chat") blink1-tool --rgb=0,255,0 --nogamma --quiet;; # Set Blink1 to rgb(0,255,0)
+        "away" | "xa") blink1-tool --rgb=255,100,0 --nogamma --quiet;; # Set Blink1 to rgb(255,100,0)
+        "dnd") blink1-tool --rgb=255,0,0 --nogamma --quiet;; # Set Blink 1 to rgb(255,0,0)
+      esac
+
+      echo "Status: ${user_current_show}"
+      echo "Message: ${user_current_status}"
+      user_last_show=${user_current_show}
+
+    fi
 
   else
     echo "You aren't logged in to HipChat"
     blink1-tool --off --quiet
     exit 1;
   fi
-
-  sleep 10
+  sleep 5
 done
 
 echo "No Blink1 device was detected"
