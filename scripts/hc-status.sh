920c623a71ed150713e2b2bdf34d7cfe9517b65f
Billy Whited <billy.whited@ampf.com>
2016-11-11 09:19:32 -0600

    update variables to use recommended syntax

--- scripts/hc-status.sh
+++ scripts/hc-status.sh
@@ -7,44 +7,44 @@ source ~/bin/hc-status.cfg
 
 blink1_devices=$(blink1-tool --list | grep -q "id:[0-9]\+"; echo $?)
 
-while  [ "$blink1_devices" == 0 ]; do
+while  [ "${blink1_devices}" == 0 ]; do
 
   echo "$(tput bold)Fetching user object via HipChat API...$(tput sgr0)"
 
   get_user=$(curl \
              --silent \
              --header "content-type: application/json" \
-             --header "Authorization: Bearer $view_token" \
-             --request GET https://api.hipchat.com/v2/user/{"$email"}
+             --header "Authorization: Bearer ${view_token}" \
+             --request GET https://api.hipchat.com/v2/user/{"${email}"}
             )
 
-  user_presence=$(echo $get_user | jq -r ".presence.is_online")
-  user_status=$(echo $get_user | jq -r ".presence.show")
+  user_presence=$(echo ${get_user} | jq -r ".presence.is_online")
+  user_status=$(echo ${get_user} | jq -r ".presence.show")
 
   get_last_color=$(blink1-tool --rgbread  --quiet)
   last_color=$(printf "rgb(%d,%d,%d)\n" ${get_last_color//,/ })
 
-  if [ "$user_presence" == "true" ]; then
+  if [ "${user_presence}" == "true" ]; then
 
-    echo "$user_presence"
-    echo "$user_status"
-    echo "$last_color"
+    echo "${user_presence}"
+    echo "${user_status}"
+    echo "${last_color}"
 
-    case $user_status in
+    case ${user_status} in
       "chat")
-        if [ "$last_color" != "rgb(0,255,0)" ]; then
+        if [ "${last_color}" != "rgb(0,255,0)" ]; then
           blink1-tool --rgb=0,255,0 --nogamma --quiet
           echo "Your status has changed to: ${user_status}"
         fi
         ;;
       "away"|"xa")
-        if [ "$last_color" != "rgb(255,100,0)" ]; then
+        if [ "${last_color}" != "rgb(255,100,0)" ]; then
           blink1-tool --rgb=255,100,0 --nogamma --quiet
           echo "Your status has changed to: ${user_status}"
         fi
         ;;
       "dnd")
-        if [ "$last_color" != "rgb(255,0,0)" ]; then
+        if [ "${last_color}" != "rgb(255,0,0)" ]; then
           blink1-tool --rgb=255,0,0 --nogamma --quiet
           echo "Your status has changed to: ${user_status}"
         fi
