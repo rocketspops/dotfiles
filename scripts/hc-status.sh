1c28197630ecca4c2f89698caa58a92627e7e8dc
Billy Whited <billy.whited@ampf.com>
2016-11-11 09:15:37 -0600

    remove old comments and detect if there is a blinkdevice connected

--- scripts/hc-status.sh
+++ scripts/hc-status.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# USAGE
-#  1. Install Homebrew (http://brew.sh/)
-#  2. Install blink1-tool: $ brew install blink1
-#  3. Create a /bin file in your home directory: $ mkdir ~/bin
-#  4. Grab a copy of hc-status.sh (https://git.io/vK6Ao) and place it in ~/bin
-#  5. Grab a copy of hc-status.cfg (https://git.io/vK6x3) and place it in ~/bin
-#  6. Add ~/bin to your $PATH (http://www.troubleshooters.com/linux/prepostpath.htm#_singleuser)
-#  7. Create 2 personal API tokens scoped to Admin Group and View Group  (https://www.hipchat.com/sign_in?d=%2Faccount%2Fapi)
-#  8. Update hc-status.cfg with your API tokens
-#  9. Get your current user info: $ sh hc-status.sh
-
 source ~/bin/hc-status.cfg
 
-while true; do
+# Check if there are any connected Blink1 devices: `grep` output of `blink1-tool --list` using
+# `-q` to silence the output, then return the exit status of the command using `echo`.
+
+blink1_devices=$(blink1-tool --list | grep -q "id:[0-9]\+"; echo $?)
+
+while  [ "$blink1_devices" == 0 ]; do
 
   echo "$(tput bold)Fetching user object via HipChat API...$(tput sgr0)"
 
@@ -68,3 +62,5 @@ while true; do
 
   sleep 10
 done
+
+echo "No Blink1 device was detected"
